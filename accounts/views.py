5dc6897d86ddb5ebf47ca86c175364bf2ed19a76
popsonebz <popoolaebenezer@gmail.com>
2017-08-11 23:45:23 +0200

    creating a login form

---
+++ accounts/views.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib.auth import (
+    authenticate,
+    get_user_model,
+    login,
+    logout,)
+
+from django.shortcuts import render
+
+from .forms import UserLoginForm
+# Create your views here.
+def login_view(request):
+    title = "Login"
+    form = UserLoginForm(request.POST or None)
+    if form.is_valid():
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+    return render(request, "accounts/form.html", {"form":form, "title":title})
+
+def register_view(request):
+    return render(request, "accounts/form.html", {})
+
+def logout_view(request):
+    return render(request, "form.html", {})
